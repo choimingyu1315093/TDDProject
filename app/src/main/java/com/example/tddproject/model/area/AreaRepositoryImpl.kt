c61a9f88feec8f9687a4f3f504dcb735cbf2ecf8
choimingyu <alsrb1315093@gmail.com>
2025-09-13 22:21:56 +0900

    TDD Base 작업

--- app/src/main/java/com/example/tddproject/model/area/AreaRepositoryImpl.kt
+++ app/src/main/java/com/example/tddproject/model/area/AreaRepositoryImpl.kt
@@ -8,11 +8,6 @@ class AreaRepositoryImpl(
     private val mapper: AreaMapper
 ): AreaRepository {
     override suspend fun getAreaCode(os: String, app: String, serviceKey: String): List<Area> {
-        try {
-            val dtos = api.getAreaCode(mobileOS = "AND", mobileApp = "APP", serviceKey = BuildConfig.SERVICE_KEY)
-            return mapper.dtoToList(dtos)
-        }catch (t: Throwable){
-            throw ErrorHandler.wrap(t)
-        }
+        return emptyList()
     }
 }
